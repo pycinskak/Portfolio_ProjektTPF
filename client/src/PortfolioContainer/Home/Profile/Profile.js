66a5783466bdfe991c2c0a148e1897dda612f48f
Kasia Pycińska <kasiapycinska@MacBook-Air-Kasia.local>
2022-02-08 20:45:58 +0100

    add portfolio client and server

---
+++ client/src/PortfolioContainer/Home/Profile/Profile.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import Typical from 'react-typical'
+import './Profile.css'
+import ScrollService from '../../../utilities/ScrollService'
+
+export default function Profile() {
+    return(
+        <div className='profile-container'>
+            <div className='profile-parent'>
+                <div className='profile-details'>
+                    <div className='colz'>
+                        <div className='colz-icon'>
+                            <a href='#'>
+                                <i className='fa fa-facebook-square'></i>
+                            </a>
+                            <a href='#'>
+                                <i className='fa fa-google-plus-square'></i>
+                            </a>
+                            <a href='#'>
+                                <i className='fa fa-instagram'></i>
+                            </a>
+                            <a href='#'>
+                                <i className='fa fa-youtube-square'></i>
+                            </a>
+                            <a href='#'>
+                                <i className='fa fa-twitter'></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div className='profile-details-name'>
+                        <span className='primary-text'>
+                            {" "}
+                            Hello, I'm <span className='highlighted-text'>Kasia</span>
+                        </span>
+                    </div>
+                    
+                    <div className='profile-details-role'>
+                        <span className='primary-text'>
+                            {" "}
+                            <h1>
+                                {" "}
+                                <Typical
+                                loop={Infinity}
+                                steps={[
+                                    "UX/UI Designer ",
+                                    1000,
+                                    "Tools Developer",
+                                    1000,
+                                    "Business Analyst",
+                                    1000,
+                                    "Photographer 📸",
+                                    1000
+                                ]} />
+                            </h1>
+                            <span className='profile-role-tagline'>
+                                Please see my portfolio! 
+                            </span>
+                        </span>
+                    </div>
+                    <div className='profile-options'>
+                        <button className='btn primary-btn' onClick={() => ScrollService.scrollHandler.scrollToHireMe()}>
+                            {" "}
+                            Hire Me{" "}
+                        </button>
+                        <a href="cv.pdf" download='KatarzynaPycinskaCV.pdf'>
+                            <button className="btn highlighted-btn">Get Resume</button>
+                        </a>
+                    </div>
+                </div>
+                <div className='profile-picture'>
+                    <div className='profile-picture-background'></div>
+                </div> 
+            </div>
+        </div>
+    )
+}
